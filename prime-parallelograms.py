0634af3b093fb59a7f8edbcbfe0b7fe2c4d919fc
Pikel1997 <76587209+Pikel1997@users.noreply.github.com>
2021-08-17 16:32:54 +0400

    changed to numpy array for time

--- prime-parallelograms.py
+++ prime-parallelograms.py
@@ -1,19 +1,22 @@
+import numpy as np
+# np.set_printoptions(threshold=1000000)
+
 def prime_numbers(n):
     global list_of_primes
     list_of_primes = []
     for num in range(2, n+1):
-        if all(num%i!=0 for i in range(2,num)):
+        if all(num % i !=0 for i in range(2, num)):
             list_of_primes.append(num)
-    print(list_of_primes)
+    print(np.array(list_of_primes))
 
 
 def bin_of_primes():
     global list_of_binary
     list_of_binary = []
     for x in list_of_primes:
-        list_of_binary.append(bin(x).replace("0b",""))
+        list_of_binary.append(bin(x).replace("0b", ""))
     list_of_binary = list(map(int, list_of_binary))
-    print(list_of_binary)
+    print(np.array(list_of_binary))
 
 
 def rev_bin_of_primes():
@@ -22,7 +25,7 @@ def rev_bin_of_primes():
     for y in list_of_binary:
         list_of_rev_binary.append(str(y)[::-1])
     list_of_rev_binary = list(map(int, list_of_rev_binary))
-    print(list_of_rev_binary)
+    print(np.array(list_of_rev_binary))
 
 
 def dec_of_rev_bin_of_primes():
@@ -30,7 +33,7 @@ def dec_of_rev_bin_of_primes():
     list_of_dec_of_rev_binary = []
     for y in list_of_rev_binary:
         list_of_dec_of_rev_binary.append(int(str(y), 2))
-    print(list_of_dec_of_rev_binary)
+    print(np.array(list_of_dec_of_rev_binary))
 
 
 n = int(input("enter number- "))
